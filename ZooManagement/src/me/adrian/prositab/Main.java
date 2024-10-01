47c0ad50f8aeb563b2b7c70f75112fbfe552518e
MagreyTheCode <adriansalvadorekomo.mesiobono@esprit.tn>
2024-10-01 07:49:00 +0100

    new version 2.0.0

--- ZooManagement/src/me/adrian/prositab/Main.java
+++ ZooManagement/src/me/adrian/prositab/Main.java
@@ -4,20 +4,32 @@ import me.adrian.prositab.utils.*;
 public class Main {
     public static void main(String[] args) {
 
-        boolean check,checkl,checke;
+        boolean check,checke,t;
         int search,s;
 
         Animal lion = new Animal("Scar", "Feline", 50, false);
+        Animal dog = new Animal("Dog", "Mamifere", 10, false);
         Animal lionn = new Animal("Scar", "Feline", 50, false);
+        Animal lionnn = new Animal("Scarface", "Feline", 20, false);
+        Animal tiger = new Animal("Milky", "Feline", 13, true);
+        Animal owl = new Animal("Quasimodo", "Bird", 8, true);
+        Animal elephant = new Animal("Terrence", "Mamifere", 9, true);
         Zoo tunis = new Zoo("International Zoo of Ariana", "Tunis");
+        Zoo sfax = new Zoo("International Zoo of Sfax", "Sfax");
 
-        checkl = tunis.addAnimal(lionn);
-        check = tunis.addAnimal(lion);
-        search = tunis.searchAnimal(lion);
-        s = tunis.searchAnimal(lionn);
-        System.out.println("Animal added: " + check + "" + checkl + tunis.counter);
-        System.out.println("Animal searched: " + search + "" + s);
 
+
+        tunis.addAnimal(lion);
+        tunis.addAnimal(lionn);
+        tunis.addAnimal(lionnn);
+        tunis.addAnimal(tiger);
+        tunis.addAnimal(owl);
+        tunis.addAnimal(elephant);
+        tunis.removeAnimal(dog);
+        sfax.addAnimal(dog);
+        Zoo.compareZoo(tunis,sfax);
         tunis.displayZoo();
+        sfax.displayZoo();
+        System.out.println(tunis.isZooFull());
     }
 }
